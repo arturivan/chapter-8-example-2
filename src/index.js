d943bbbe4ed13388e970db70b372e391d847d750
Artur Ivan <arturivan.hu@gmail.com>
2024-08-26 16:54:05 +0200

    update

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ if (require.main === module) {
     //
     // When this script is run as the entry point, starts the HTTP server.
     //
-    app.listen(PORT, () => {
+    app.liste(PORT, () => {
         console.log(`Microservice online.`);
     });
 }
